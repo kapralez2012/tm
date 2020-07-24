89c783e6dd73f7e76cdb67b024edfa66b2fc6296
kapralez2012 <kapralez2012@gmail.com>
2020-07-24 11:56:13 +0200

    Moj pierwszy projekt w C

---
+++ tm.c
@@ -0,0 +1,20 @@
+/*
+ * tm.c
+ *
+ *  Created on: Jul 23, 2020
+ *      Author: kapralez2012
+ */
+
+#include <stdio.h>
+#include "tm.h"
+int main()
+{
+
+	// make two process which run same
+	// program after this instruction
+
+
+	printf("Hello world!\n");
+	return 0;
+}
+
